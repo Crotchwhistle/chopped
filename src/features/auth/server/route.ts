eb9784b336209f692c3e9db3d9f7644979e55d4b
Suhaan Sharma <subaanfeetpics@gmail.com>
2025-01-09 01:04:08 +1100

    building a session middleware

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -5,11 +5,21 @@ import { deleteCookie, setCookie } from "hono/cookie";
 import { zValidator } from "@hono/zod-validator";
 
 import { createAdminClient } from "@/lib/appwrite";
+import { sessionMiddleware } from "@/lib/session-middleware";
 
 import { AUTH_COOKIE } from "../constants";
 import { loginSchema, registerSchema } from "../schemas";
 
 const app = new Hono()
+    .get(
+        "/current", 
+        sessionMiddleware, 
+        (c) => {
+            const user = c.get("user");
+
+            return c.json({ data: user });
+        }
+    )
     .post(
         "/login", 
         zValidator("json", loginSchema), 
@@ -65,8 +75,12 @@ const app = new Hono()
     )
     .post(
         "/logout",
-        (c) => {
+        sessionMiddleware,
+        async (c) => {
+            const account = c.get("account");
+
             deleteCookie(c, AUTH_COOKIE)
+            await account.deleteSession("current")
 
             return c.json({ success: true });
         }

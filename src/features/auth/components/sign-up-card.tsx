1d449fc512952feefb387fd8ca70c5109d264ce7
Suhaan Sharma <subaanfeetpics@gmail.com>
2025-01-09 00:02:27 +1100

    creating auth api

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -23,15 +23,14 @@ import {
     FormMessage,
 } from "@/components/ui/form"
 
-const formSchema = z.object({
-    name: z.string().trim().min(1, "Name is required"),
-    email: z.string().email(),
-    password: z.string().min(8, "Minimum of 8 characters 🫣"),
-})
+import { registerSchema } from "../schemas"
+import { useRegister } from "../api/use-register"
 
 export const SignUpCard = () => {
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+    const { mutate } = useRegister()
+
+    const form = useForm<z.infer<typeof registerSchema>>({
+        resolver: zodResolver(registerSchema),
         defaultValues: {
             name: "",
             email: "",
@@ -39,8 +38,8 @@ export const SignUpCard = () => {
         }
     })
 
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
-            console.log({ values })
+    const onSubmit = (values: z.infer<typeof registerSchema>) => {
+        mutate({ json: values })
     }
 
     return (
